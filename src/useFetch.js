24bd9b3f97546efcdba5c3997c9c618c69faa51f
javenkn <javenkn7@gmail.com>
2020-01-12 14:54:19 -1000

    Creates fetch hook for reusability

---
+++ src/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function useFetch(url, options) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url, options);
+        const data = await res.json();
+        data.sort((a, b) => {
+          // sorts data alphabetically by name
+          if (a.name > b.name || a.title > b.title) return 1;
+          if (a.name < b.name || a.title < b.title) return -1;
+          return 0;
+        });
+        setData(data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err);
+      }
+    };
+    fetchData();
+  }, [url, options]);
+
+  return { isLoading, data, error };
+}
+
+export default useFetch;
